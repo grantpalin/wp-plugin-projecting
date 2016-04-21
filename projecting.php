ded945c8f0cfb650f79c5b2e9b4bdd5dd6542b4e
Grant Palin <grant@grantpalin.com>
2016-04-21 00:02:11 -0700

    Added ability to display number of projects in the dashboard At a Glance widget.

--- projecting.php
+++ projecting.php
@@ -76,3 +76,29 @@ function projecting_project() {
 	register_post_type( 'project', $args );
 }
 add_action( 'init', 'projecting_project', 0 );
+
+function projecting_enqueue_styles() {
+	wp_enqueue_style('projecting-admin', plugins_url('assets/projecting-admin.css', __FILE__), array(), '1.0.0');
+}
+add_action('admin_enqueue_scripts', 'projecting_enqueue_styles');
+
+// Add custom content types to the 'At a Glance' dashboard widget
+function projecting_right_now_content_table_end() {
+	$num_posts = wp_count_posts( 'project' );
+
+	// Only proceed if one or more published instances of this content type
+	if ( $num_posts && $num_posts->publish ) {
+		// Create the plural or singular text matching the count, then add the count to the string
+		$text = sprintf( _n( '%s Project', '%s Projects', $num_posts->publish ), $num_posts->publish );
+
+		$post_type_object = get_post_type_object( 'project' );
+
+		// Output a link only if the current user is able to edit content of this type
+		if ( $post_type_object && current_user_can( $post_type_object->cap->edit_posts ) ) {
+			printf( '<li class="project-count"><a href="edit.php?post_type=%1$s">%1$s</a></li>', $text );
+		} else { // Else output just the number and text
+			printf( '<li class="project-count"><span>%1$s</span></li>', $text );
+		}
+	}
+}
+add_action( 'dashboard_glance_items' , 'projecting_right_now_content_table_end' );
